4cc7bd50d2e7e0c549f0756bd2fcaecefdc54f5d
HarrisonBrookesBoon <harrison.m.brookes@gmail.com>
2022-10-31 21:55:13 +0000

    Add loadJson function and related inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
-console.log("Hello, world!")
+//Constants and variables
+
+//Initialisation
+
+
+//Functions
+function loadJson() {
+    jsonInput = document.getElementById('file-selector');
+
+    if (!jsonInput.files[0]) {
+        alert("Please select a file before clicking 'Load'")
+    }
+    else {
+        file = jsonInput.files[0];
+        reader = new FileReader();
+        reader.onload = parseText;
+        reader.readAsText(file);
+    }
+
+    function parseText(e) {
+        let lines = e.target.result;
+        let text = "";
+        var faultsJson = JSON.parse(lines);
+        console.log(faultsJson);
+        devicesList = faultsJson.devices;
+        document.getElementById("help-prompt").innerHTML = "Now please choose a device type";
+
+    }
+
+}

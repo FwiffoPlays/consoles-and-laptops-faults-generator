98d332e121a284a342d91682dc473a36ddaff577
HarrisonBrookesBoon <harrison.m.brookes@gmail.com>
2022-10-31 23:23:34 +0000

    Add the displayDevices function

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,29 @@ function loadJson() {
         console.log(faultsJson);
         devicesList = faultsJson.devices;
         document.getElementById("help-prompt").innerHTML = "Now please choose a device type";
-
+        displayDevices();
     }
 
+}
+
+function displayDevices() {
+    devicesDiv = document.getElementById("devices-div"); //Find the devices div
+    for (device of devicesList) { //Iterate through the devices array and create a button for each device type
+        devicesDiv.innerHTML += "<button id='"+device+"Button"+"'>"+device+"</button>";
+    }
+    devicesDivContent = document.getElementsByClassName("devices")[0];
+    buttons = devicesDiv.getElementsByTagName("button");//Retrieve a list of buttons within the devices-div
+    
+    for (button of buttons) {
+        button.addEventListener("click", function() {
+            buttonType = this.getAttribute("id");
+            alert("You clicked the "+buttonType+" button");
+            //displayFaults(buttonType);
+        });
+    }
+
+        
+
+
+       
 }
